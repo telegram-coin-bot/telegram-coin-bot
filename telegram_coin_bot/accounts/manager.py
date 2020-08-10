0660bdd8e3e8302aceb738e5ec6f3a53bdfb0450
Wild Print <wild.print@rambler.ru>
2020-08-10 20:27:00 +0500

    add class for managing accounts (can turn on/off visiting tasks)

---
+++ telegram_coin_bot/accounts/manager.py
@@ -0,0 +1,33 @@
+from telegram_coin_bot.db.schema import Session
+from telegram_coin_bot.handlers import visit_sites
+from telegram_coin_bot.bot import create_bot
+import asyncio
+from telegram_coin_bot.utils.config import Config
+
+
+class AccountsManager:
+    def __init__(self):
+        self.accounts = []
+        self.tasks = []
+        self.started = False
+
+    async def start(self, loop):
+        if self.started:
+            return
+        self.started = True
+        self.accounts = [
+            await create_bot(
+                session.session_string, session.api_id, session.api_hash, session.phone,
+            )
+            for session in Session.select()
+        ]
+        await asyncio.gather(
+            *[
+                account.send_message(Config.BOT_ADDRESS.value, "/menu")
+                for account in self.accounts
+            ]
+        )
+
+    async def stop(self):
+        await asyncio.gather(*[account.disconnect() for account in self.accounts])
+        self.started = False
